eada67c0ea3971f98befd970c9464c14b8c007d1
fotis_pozoglou <fotispozoglou@protonmail.com>
2022-05-31 23:42:40 +0300

    Added correct api domain in config

--- public/scripts/config/config.js
+++ public/scripts/config/config.js
@@ -1,3 +1,3 @@
 export const SERVER_IP = '192.168.1.6';
 export const SERVER_URL = `http://${ SERVER_IP }:8080`;
-export const API_SERVER_URL = `http://${ SERVER_IP }:3000`;
+export const API_SERVER_URL = `https://app-efood-api.herokuapp.com`;

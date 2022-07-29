d8381c1ebbd137e603079f53a06406efc879d802
fotis_pozoglou <fotispozoglou@protonmail.com>
2022-07-30 00:02:53 +0300

    Configuring For Heroku Deployment

--- public/scripts/config/config.js
+++ public/scripts/config/config.js
@@ -1,3 +1,3 @@
 export const SERVER_IP = '192.168.1.8';
-export const SERVER_URL = `https://efood-client.herokuapp.com`;
+export const SERVER_URL = `https://efood-admin.herokuapp.com`;
 export const API_SERVER_URL = `https://efoodapi.herokuapp.com`;
